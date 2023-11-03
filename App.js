93a0ca97c7b3c492d43e123bac9eac3853c7d080
Arthur Uezu <uezuarthur@gmail.com>
2023-11-03 13:01:57 -0300

    Camera v1

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CameraComponent from './components/CameraComponent';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+      <CameraComponent></CameraComponent>
+      // <StatusBar style="auto" />
+    // </View>
   );
 }
 
@@ -14,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    // height: 
   },
 });
